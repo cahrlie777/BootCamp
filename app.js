395889de741e2fd4c92f949807eb2a792b7d92f6
Charlie Cruz <cru16157@uvg.edu.gt>
2020-09-27 19:01:42 -0600

    Mongo database added

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
+const express    = require("express"),
+      app        = express(),
+      bodyParser = require("body-parser"),
+      mongoose   = require("mongoose");
 
 
 //Confuguration
 app.use( bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-let campgrounds = [
-    {name:"Charlie Cruz", image:"https://ubiqum.com/assets/uploads/2019/03/untitled-design-1-1-1.png"},
-    {name:"Charlie Cruz", image:"https://ubiqum.com/assets/uploads/2019/03/untitled-design-1-1-1.png"},
-    {name:"Charlie Cruz", image:"https://ubiqum.com/assets/uploads/2019/03/coding_bootcamp.jpg"}
-  ];
+mongoose.connect('mongodb://localhost/bootcamp', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+});
+
+//Scheama setup
+let campgroundSchema =  new mongoose.Schema({
+   name: String,
+   image: String
+});
+
+let Campground = mongoose.model("Campground",campgroundSchema);
+
 
 //Routes
 app.get("/", (req, res) =>{
   res.render("landing")
 })
 
-app.get("/campgrounds", function(req, res){
-  res.render("campgrounds", {campgrounds: campgrounds});
-})
+app.get("/campgrounds", (req, res) => {
+  Campground.find({}, (err, allcampgrounds) =>{
+    if(err){
+
+    }else{
+    res.render("campgrounds", {campgrounds: allcampgrounds});
+    }
+  })
+});
 
 app.get("/campgrounds/new",(req, res) =>{
   res.render("new.ejs")
@@ -30,8 +47,13 @@ app.post("/campgrounds",(req, res)=>{
   let name = req.body.name;
   let image = req.body.image;
   let newCampground = {name: name, image: image};
-  campgrounds.push(newCampground);
-  res.redirect("/campgrounds");
+  Campground.create(newCampground,(err,camground)=>{
+    if(err){
+      console.log(err);
+    }else{
+      res.redirect("/campgrounds");
+    }
+  });
 });
 
 
